5fb4a543d47f8a423abd35b0ff23805832adf5a2
ivanbeshka <ivanbeshka2002@gmail.com>
2019-07-08 16:52:32 +0500

    Дз №7

---
+++ src/Lesson7/Server/Server.java
@@ -0,0 +1,71 @@
+package Lesson7.Server;
+
+import com.sun.deploy.util.ArrayUtil;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.SQLException;
+import java.util.Vector;
+
+public class Server {
+    Vector <ClientHandler> clients;
+
+    public Server() throws SQLException {
+        AuthService.connect();
+//        System.out.println(AuthService.getNickByLoginAndPass("login1","pass1"));
+
+        ServerSocket server = null;
+        Socket socket = null;
+
+        try {
+            clients = new Vector<>();
+            server = new ServerSocket(8189);
+            System.out.println("Сервер запущен");
+
+            while(true){
+                socket = server.accept();
+                System.out.println("Клиент подключился");
+                new ClientHandler(this,socket);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            AuthService.disconnect();
+        }
+    }
+
+    public void broadcastMsg(String str){
+        for (ClientHandler o: clients) {
+            o.sendMsg(str);
+        }
+    }
+
+    public void lMsg(String [] strNick){
+        for (ClientHandler o: clients) {
+            if(o.getNick() == strNick[1]){
+                String str = String.join(" ",strNick);
+                o.sendMsg(str);
+            }
+        }
+    }
+
+    public void subscribe(ClientHandler clientHandler){
+        clients.add(clientHandler);
+    }
+
+    public void unsubscribe(ClientHandler clientHandler){
+        clients.remove(clientHandler);
+    }
+}
